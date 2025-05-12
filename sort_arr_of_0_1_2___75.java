86a212dd9c7a53d4f26e5a64fdd718a9984679ae
Rahul Rai <rairayrahul22@gmail.com>
2025-05-12 15:11:50 +0530

    12\05\2025

--- sort_arr_of_0_1_2___75.java
+++ sort_arr_of_0_1_2___75.java
@@ -12,6 +12,7 @@ public class sort_arr_of_0_1_2___75 {
         int n=arr.length;
         int c0=0,c1=0,c2=0;
 
+        // count of 0,1,2
         for(int i=0;i<n;i++){
             if(arr[i]==0){
                 c0++;
@@ -35,6 +36,35 @@ public class sort_arr_of_0_1_2___75 {
         for(i=c0+c1;i<c0+c1+c2;i++){
             arr[i]=2;
         }
+    }
+
+    public static void method2(int[] arr) { // O(n)  O(1)
+        // code here
+        int n=arr.length;
+        int low=0, mid=0, high=n-1;
+
+        while(mid<=high){
+            if(arr[mid]==0){
+                swap(arr,mid,low);
+                low++;
+                mid++;
+            }
+
+            else if(arr[mid] ==1){
+                mid++;
+            }
+
+            else{
+                swap(arr,mid,high);
+                high--;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
     public static void print(int[] arr) {
