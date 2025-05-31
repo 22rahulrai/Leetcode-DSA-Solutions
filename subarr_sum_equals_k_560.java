2e219df8b00f41e014355eff4203c1f02fb0c31d
Rahul Rai <rairayrahul22@gmail.com>
2025-05-31 10:27:09 +0530

    31/05/2025

--- subarr_sum_equals_k_560.java
+++ subarr_sum_equals_k_560.java
@@ -1,11 +1,14 @@
 package questions.leetcode;
 
+import java.util.HashMap;
+
 public class subarr_sum_equals_k_560 {
     public static void main(String[] args) {
-        System.out.println(STR."using method1 tc=O(n^2)  \{method1(new int[]{1,2,1,2,1},3)}");
-        System.out.println(STR."\{method1(new int[]{1,2,3},3)}");
+        System.out.println(STR."using method1 \n \{method1(new int[]{1,1,1},2)}   \ntc=O(n^2) ");
+        System.out.println(STR."using method2 \n \{method2(new int[]{3,1,2,4},6)}   \nTC=0(n)");
     }
 
+    // ----------------------  better solution  ---------------------------------------//
     public static int method1(int[] nums, int t) { // O(n^2)
         int n = nums.length;
         int c = 0;
@@ -19,5 +22,22 @@ public class subarr_sum_equals_k_560 {
             }
         }
         return c;
+    }
+
+    // ----------------------------------    Optimal solution  ------------------------------//
+    public static int method2(int []arr, int k){ // Tc = 0(n)  SPace= o(n)
+        int n=arr.length;
+        int sum=0, count=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(0,1);
+
+        for (int j : arr) {
+            sum += j;
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
     }
 }
